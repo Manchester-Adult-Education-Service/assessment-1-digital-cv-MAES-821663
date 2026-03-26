0f012407db92a66f05615d23c48365551b24b6c0
MAES-821663 <821663@manadulted.ac.uk>
2026-03-26 14:23:08 +0000

    Implementing more Javasript

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const headerBar = document.querySelector("header")
 const subTitles = document.querySelectorAll("h2");
 const Year = new Date().getFullYear();
 
+const quoteBtn = document.getElementById("quote-btn");
+const quoteText = document.getElementById("quote-text");
+const quoteAuthor = document.getElementById("quote-author");
+
 if (hour < 12) {
     nameHeading.textContent = "Good Morning, Jack"
 } else if (hour < 18) {
@@ -13,6 +17,15 @@ if (hour < 12) {
     nameHeading.textContent = "Good Evening, Jack"
 }
 
+quoteBtn.addEventListener("click", function(){
+    quoteText.textContent = "Fetching..."
+    fetch("https://dummyjson.com/quotes/random")
+    .then(Response => Response.json())
+    .then(data => {
+        quoteText.textContent = '${data.quote}';
+        quoteAuthor.textContent = '${data.author}';
+    });
+});
 
 console.log ("The current hour is: " + 13);
 
